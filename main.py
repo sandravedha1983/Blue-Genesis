556192588428a978b82ea8cedd22ac85d199ebe7
sandravedha1983 <sandravedha1983@gmail.com>
2026-03-25 02:00:30 +0530

    Fix: Revert backend port to 8000 and add .gitignore

--- main.py
+++ main.py
@@ -25,4 +25,4 @@ def root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="localhost", port=5500, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
